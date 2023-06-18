3048a80ad38e8cc775f68e7b0b5152b9d1e484ed
ginos1998 <ginosiam1998@gmail.com>
2023-06-18 01:11:37 -0300

    grafico en tiempo real y responsivo a los botones A y B (cambiar la info que se muestra de acuerdo al boton presionado). Lo unico que, por alguna razon, no se puede cambiar el titulo en tiempo real. F. Sale lol

--- GUI/src/main/java/sdc/tp5/gui/utils/Constants.java
+++ GUI/src/main/java/sdc/tp5/gui/utils/Constants.java
@@ -1,10 +1,15 @@
 package sdc.tp5.gui.utils;
 
 public class Constants {
+
+  private Constants() {
+    // default constructor
+  }
   public static final String FILE_PATH = "/proc/meminfo";
   public static final int UPDATE_INTERVAL_MS = 500;
   public static final String WINDOW_TITTLE = "Monitor de Memoria RAM";
-  public static final String GRAPH_TITLE = "Memoria RAM Disponible";
+  public static final String GRAPH_TITLE_AV_RAM = "Memoria RAM Disponible";
+  public static final String GRAPH_TITLE_FREE_RAM = "Memoria RAM Libre";
   public static final String BUTTON_A = "Boton A";
   public static final String BUTTON_B = "Boton B";
 
