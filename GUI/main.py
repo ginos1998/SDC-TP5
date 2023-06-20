46b7892cd0f6560bdb2f590013060fc6d2210586
ginos1998 <ginosiam1998@gmail.com>
2023-06-20 15:31:26 -0300

    cambios de logica

--- GUI/main.py
+++ GUI/main.py
@@ -9,7 +9,6 @@ file_path = "/dev/my_gpio_device"
 def botonHandler():
     root.update_idletasks()  # Actualizar la ventana para que la etiqueta desaparezca de inmediato
     lectura = "0"
-    etiqueta.config(text="presiona el boton...")
     num = random.randint(0, 3)
 
     while num > 0:
@@ -25,7 +24,7 @@ def sensorHandler():
     escribirArchivo(lectura)
 
 
-def procesarArchivo(x):
+def procesarBoton(x):
     etiqueta.config(text="")
     root.update_idletasks()  # Actualizar la ventana para que la etiqueta desaparezca de inmediato
 
@@ -38,6 +37,18 @@ def procesarArchivo(x):
 
     root.after(3000, lambda: etiqueta.config(text=""))  # Eliminar el texto después de 3 segundos
 
+def procesarSensor(x):
+    etiqueta.config(text="")
+    root.update_idletasks()  # Actualizar la ventana para que la etiqueta desaparezca de inmediato
+
+    if x == "-1" or x == -1:
+        etiqueta.config(text="UPS! algo salio mal..")
+    elif x == "0" or x == 0:
+        etiqueta.config(text="se han tomado todo el vino ohohohh")
+    elif x == "1" or x == 1:
+        etiqueta.config(text="como te tiene tu jermuu")
+
+    root.after(3000, lambda: etiqueta.config(text=""))  # Eliminar el texto después de 3 segundos
 
 def escribirArchivo(opt):
     with open(file_path, "w") as file:
@@ -49,31 +60,33 @@ def escribirArchivo(opt):
 
 def leerArchivo():
     estado = 1
-    # estado_aux = 0
     time_aux = 0
     start_time = time.time()
+    entrada = ""
     while time.time() - start_time < 5:
         with open(file_path, "r") as file:
             content = file.read().strip()
+            entrada = content
             if content.startswith("BOTON:") or content.startswith("ALCOHOLIMETRO:"):
                 estado = content.split(":")[1].strip()
 
                 if estado == 0:
                     time_aux = time.time() - start_time
 
-                #if estado == 1:
-                #    estado_aux = 1
+                if estado == 1:
+                    break
 
         time.sleep(1)
 
     file.close()
 
-    #estado = estado_aux
+    if lectura == "0" and estado == 1:  # si apreto el boton y supero el tiempo de rta (0.5 s)
+        estado = 1 if time_aux > 1 else 0
 
-    if lectura == 0 and estado == 1:  # si apreto el boton y supero el tiempo de rta (0.5 s)
-        estado = 1 if time_aux > 0.5 else 0
-
-    procesarArchivo(estado)
+    if entrada.startswith("BOTON:"):
+        procesarBoton(estado)
+    elif entrada.startswith("ALCOHOLIMETRO:"):
+        procesarSensor(estado)
 
 
 root = tk.Tk()
